5a526080bf4009e767b669436f18398671ba0104
Kaustubh Aher <aherkaustubhramesh16@gmail.com>
2025-01-27 15:56:53 +0530

    romove spaces

---
+++ String/RemoveSpacesFromString.java
@@ -0,0 +1,8 @@
+package DSA.String;
+
+public class RemoveSpacesFromString {
+    public static void main(String[] args) {
+        String t = "Hi There";
+        System.out.println(t.replace(" ",""));
+    }
+}
