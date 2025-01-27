fb383bab4aff2cf1514f44e4ac85d3f409cd34cb
Kaustubh Aher <aherkaustubhramesh16@gmail.com>
2025-01-27 15:50:00 +0530

    palindrome check

---
+++ String/PalindromeCheck.java
@@ -0,0 +1,17 @@
+package DSA.String;
+
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String t = sc.next();
+        StringBuilder sb = new StringBuilder(t).reverse();
+        if(t.equals(sb.toString())){
+            System.out.println("String is Palindrome");
+        }
+        else{
+            System.out.println("String is Not Palindrome");
+        }
+    }
+}
--- String/ReverseStringUsingForLoop.java
+++ String/ReverseStringUsingForLoop.java
@@ -3,9 +3,11 @@ package DSA.String;
 public class ReverseStringUsingForLoop {
     public static void main(String[] args) {
         String t = "Kaustubh";
+        String reversed="";
 //        System.out.println(s.length());
         for (int i=t.length()-1;i>=0;i--){
-            System.out.print(t.charAt(i));
+            reversed = reversed + t.charAt(i);
         }
+            System.out.print(reversed);
     }
 }
