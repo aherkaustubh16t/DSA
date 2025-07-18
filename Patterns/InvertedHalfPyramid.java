d87429ede31220769088a958504d2185488ab304
Kaustubh Aher <aherkaustubhramesh16@gmail.com>
2025-07-18 07:03:07 +0530

    Inverted Half Pyramid Pattern problem solved

--- Patterns/InvertedHalfPyramid.java
+++ Patterns/InvertedHalfPyramid.java
@@ -2,12 +2,9 @@ package DSA.Patterns;
 
 public class InvertedHalfPyramid {
     public static void main(String[] args) {
-        int t =4;
-        for (int i = 1; i <= t; i++) {
-            for (int j = 1;j<=t-i;j++){
-                System.out.print(" ");
-            }
-            for(int k = 1;k<=i;k++){
+        int n = 4;
+        for(int i=0; i<n; i++){
+            for(int j=4; j>i; j--){
                 System.out.print("*");
             }
             System.out.println();
