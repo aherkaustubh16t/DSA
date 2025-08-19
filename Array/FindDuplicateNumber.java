a7304afa02ea9989b6108ffcc8fd5af906c03f73
Kaustubh Aher <aherkaustubhramesh16@gmail.com>
2025-08-19 20:44:58 +0530

    updated

--- Array/FindDuplicateNumber.java
+++ Array/FindDuplicateNumber.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class FindDuplicateNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of Array:- ");
+        System.out.println("Enter the size of Array - ");
         int n = sc.nextInt();
         int[] arr = new int[n];
 
