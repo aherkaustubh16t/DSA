d177d44a3e5922a4613682bd8f5a6ba8a20784f7
Kaustubh Aher <aherkaustubhramesh16@gmail.com>
2025-08-19 20:46:17 +0530

    updated

--- Patterns/InvertedFullPyramid.java
+++ Patterns/InvertedFullPyramid.java
@@ -2,7 +2,7 @@ package DSA.Patterns;
 
 public class InvertedFullPyramid {
     public static void main(String[] args) {
-        int n= 4;
+        int n = 4;
         for(int i=0; i<n; i++){
             for(int s=0; s<i; s++){
                 System.out.print(" ");
