6356d8b2f062c46df1f1bf3831fda807ad2d94f0
Kaustubh Aher <aherkaustubhramesh16@gmail.com>
2025-08-19 20:45:28 +0530

    updated

--- Array/PairSumInArray.java
+++ Array/PairSumInArray.java
@@ -5,7 +5,7 @@ public class PairSumInArray {
         for(int i =0;i< nums.length;i++){
             for(int j=i+1;j< nums.length;j++){
                 if(nums[i]+nums[j]==target){
-                    System.out.println("Pair Found:- "+nums[i]+","+ nums[j]);
+                    System.out.println("Pair Found - "+nums[i]+","+ nums[j]);
                 }
 
             }
